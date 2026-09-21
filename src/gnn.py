832cab31a0606fbddb33c295d3f451c991fdcc4e
Ethan Cochran <ethan@solvticians.com>
2026-09-20 20:23:19 -0500

    Init commit in ethan branch.

---
+++ src/gnn.py
@@ -0,0 +1,11 @@
+class GNN():
+    
+    def __init__():
+        pass
+    def forward():
+        pass
+    def encode():
+        pass
+    def reset_params():
+        pass
+    
---
+++ src/train.py
@@ -0,0 +1,11 @@
+import torch
+
+def train_epoch():
+    pass
+
+def train():
+    pass
+
+@torch.no_grad()
+def eval():
+    pass
